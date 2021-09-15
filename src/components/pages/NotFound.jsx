faf109c01e521cd232b04f271612ee254377783d
Sean Redmon <seen360@yahoo.com>
2021-09-15 07:32:30 -0400

    added not found page styling

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -3,9 +3,16 @@ import BackButton from "../common/BackButton";
 
 const NotFound = () => {
   return (
-    <div>
-      <BackButton /> <h1>404 Not Found</h1>
-    </div>
+    <section className="page-404 forground-color">
+      <div className="caveman-image">
+        <h1 className="text-404">404</h1>
+      </div>
+      <div className="content-box-404">
+        <h3 className="looks-like-you-are-lost">Looks like you're lost</h3>
+        <p>the page you are looking for is not available!</p>
+        <BackButton />
+      </div>
+    </section>
   );
 };
 
