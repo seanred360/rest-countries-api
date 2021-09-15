b6db1aef592db329bb352a7c8c1230a1a570b9c7
Sean Redmon <seen360@yahoo.com>
2021-09-15 11:00:06 -0400

    Heroku changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,17 +9,17 @@ const Main = () => {
     <Switch>
       {/* The Switch decides which component to show based on the current URL.*/}
       <Route
-        basename="/public"
+        // basename="/public"
         exact
         path="/not-found"
         component={NotFound}
       ></Route>
       <Route
-        basename="/public"
+        // basename="/public"
         path="/:id"
         render={(props) => <SingleCountryPage {...props} />}
       ></Route>
-      <Route basename="/public" exact path="/" component={HomePage}></Route>
+      <Route exact path="/" component={HomePage}></Route>
     </Switch>
   );
 };
